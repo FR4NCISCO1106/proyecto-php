caa0e1cf74c795e9aca34e775ab6ab0430a079af
FR4NCISCO1106 <francisquito1120@gmail.com>
2025-03-21 09:15:37 -0400

    isajdai

--- modificar_logica.php
+++ modificar_logica.php
@@ -1,5 +1,5 @@
 <?php include("./conexion.php");
-$sql = "UPDATE usuarios SET id_mascota='$_POST[id_mascota]', nombre='$_POST[nombre]',tipo_mascota='$_POST[tipo_mascota]',raza='$_POST[raza]',sexo='$_POST[sexo]', nombre_cliente='$_POST[nombre_cliente]', fecha_nacimiento='$_POST[fecha_nacimiento]'";
+$sql = "UPDATE usuarios SET nombre='$_POST[nombre]',tipo_mascota='$_POST[tipo_mascota]',raza='$_POST[raza]',sexo='$_POST[sexo]', nombre_cliente='$_POST[nombre_cliente]', fecha_nacimiento='$_POST[fecha_nacimiento]'WHERE id_mascota='$_POST[oculto]'";
 $result = mysqli_query($link, $sql);
 if (!mysqli_error($link)) {
 ?>
